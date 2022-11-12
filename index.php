db02667956f082da3121d295ffd451cb5e60cc50
Command_String <bmxmikey2@gmail.com>
2022-11-12 02:33:56 -0500

    Forgot to readd index.php file

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 
 use Discord\Bot\Config;
 use Discord\Bot\Events\ready;
+use Discord\Bot\Events\VOICE_STATE_UPDATE;
 use Discord\Discord;
 use Discord\WebSockets\Intents;
 
 require_once "./vendor/autoload.php";
 
 $config = new Config();
+
+
+#############################
+#   ENVIRONMENT VARIABLES   #
+#############################
+$config->stationsPlaying = new stdClass;
 
 $config->discord = new Discord([
     "token" => $config->token,
@@ -16,5 +23,6 @@ $config->discord = new Discord([
 ]);
 
 (new ready)->listen();
+(new VOICE_STATE_UPDATE)->listen();
 
 $config->discord->run();
